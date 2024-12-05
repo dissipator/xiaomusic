b907dcd2a427e4787d1d649444fc5ed52f8619e5
lucas <dissipator_520@qq.com>
2024-12-05 15:05:46 +0800

    add qq playlist

--- plugins/getmy_playlist.py
+++ plugins/getmy_playlist.py
@@ -16,9 +16,7 @@ import requests
 # +        url = jsons['data'][0]['url']
 
 
-async def getmy_playlist(
-    type="netease", api_host="http://127.0.0.1/api", playlist_id=None, uid=None
-):
+async def getmy_playlist(type="netease",api_host="http://127.0.0.1:3000", playlist_ids=[],uid=None,br=128000):
     """
     Purpose:
     """
@@ -33,7 +31,7 @@ async def getmy_playlist(
 
             music_list = response.json()
             for item in music_list["playlist"]:
-                list_name = item.get("name")
+                list_name = item.get("name") + "-WY"
 
                 log.info(f"getmy_playlist name:{list_name}")
                 # if item.get("id") in [12709941656,]:
@@ -56,7 +54,7 @@ async def getmy_playlist(
                     album = music["al"]["name"]
 
                     name = music.get("name")
-                    url = f"{api_host}/song/url?id={music['id']}&br=350000&realIP=211.161.244.70&proxy=HTTP:%2F%2F127.0.0.1:8080"
+                    url = f"{api_host}/song/url?id={music['id']}&br={br}&realIP=211.161.244.70&proxy=HTTP:%2F%2F127.0.0.1:8080"
                     if (not name) or (not url):
                         continue
                     xiaomusic.all_music[name] = url
@@ -79,45 +77,118 @@ async def getmy_playlist(
                 log.debug(xiaomusic.all_music)
                 log.debug(xiaomusic.music_list)
             return
-        if playlist_id:
-            songs_url = f"{api_host}/playlist/detail?id={playlist_id}"
-            # 发起请求
-            response = requests.get(songs_url, timeout=5)  # 增加超时以避免长时间挂起
-            response.raise_for_status()  # 如果响应不是200，引发HTTPError异常
+        if playlist_ids:
+            for playlist_id in playlist_ids:
+                songs_url = f"{api_host}/playlist/detail?id={playlist_id}"
+                # 发起请求
+                response = requests.get(songs_url, timeout=5)  # 增加超时以避免长时间挂起
+                response.raise_for_status()  # 如果响应不是200，引发HTTPError异常
+                # log.info(f"getmy_playlist url:{api_url} response:{response.text}")
+                musics = response.json()
+                list_name = musics['playlist']['name']
+                one_music_list = []
+                for music in musics['playlist']['tracks']:
+                    if (not music):
+                        continue
+                    # try:
+                    name = music['name']
+                    picUrl = music['al']['picUrl']
+                    artist = music['ar'][0]['name']
+                    album = music['al']['name']
 
-            musics = response.json()
-            list_name = musics["playlist"]["name"]
-            log.info(f"getmy_playlist list_name:{list_name} ")
-            one_music_list = []
-            for music in musics["playlist"]["tracks"]:
-                if not music:
-                    continue
-                # try:
-                name = music["name"]
-                picUrl = music["al"]["picUrl"]
-                artist = music["ar"][0]["name"]
-                album = music["al"]["name"]
-
-                name = music.get("name")
-                url = f"{api_host}/song/url?id={music['id']}&br=350000&proxy=HTTP:%2F%2F127.0.0.1:8080"
-                if (not name) or (not url):
-                    continue
-                xiaomusic.all_music[name] = url
-                xiaomusic.all_music_tags[name] = {
-                    "title": name,
-                    "artist": artist,
-                    "album": album,
-                    "year": "",
-                    "genre": "",
-                    "picture": picUrl,
-                    "lyrics": "",
-                }
-                one_music_list.append(name)
-            log.debug(f"getmy_playlist name:{list_name}")
-            log.debug(one_music_list)
-            # 歌曲名字相同会覆盖
-            xiaomusic.music_list[list_name] = one_music_list
-            xiaomusic.try_save_tag_cache()
+                    name = music.get("name")
+                    url = f"{api_host}/song/url?id={music['id']}&br={br}&realIP=211.161.244.70&proxy=HTTP:%2F%2F127.0.0.1:8080"
+                    if (not name) or (not url):
+                        continue
+                    xiaomusic.all_music[name] = url
+                    xiaomusic.all_music_tags[name] =  {
+                            "title": name,
+                            "artist": artist,
+                            "album": album,
+                            "year": "",
+                            "genre": "",
+                            "picture": picUrl,
+                            "lyrics": ""
+                        }
+                    one_music_list.append(name)
+                log.debug(f"getmy_playlist name:{list_name}")
+                log.debug(one_music_list)
+                # 歌曲名字相同会覆盖
+                xiaomusic.music_list[list_name] = one_music_list
+                xiaomusic.try_save_tag_cache()
+                log.debug(xiaomusic.all_music)
+                log.debug(xiaomusic.music_list)
             return
-    else:
-        log.error(f"getmy_playlist type:{type} not support")
+    if type == "qq":
+        for playlist_id in playlist_ids:
+            index = 0
+            url = f"http://i.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&utf8=1&disstid={playlist_id}&loginUin=0"
+            headers = {
+                "Referer": "https://y.qq.com/n/yqq/playlist",
+                "Cookie": "uin=",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            }
+            
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # 如果请求出现4xx、5xx等错误状态码则抛出异常
+            # print(response.text)
+            result = response.text
+            # Remove callback function wrapper and parse JSON
+            json_data = re.sub(r'^callback\(|MusicJsonCallback\(|jsonCallback\(|\)$', '', result)
+            res = json.loads(json_data)
+            one_music_list = []
+            # Extract songlist and format music items (implement formatMusicItem function)
+            if 'cdlist' in res and len(res['cdlist']) > 0:
+                # print(res['cdlist'][0]['songlist'])
+                list_name = res['cdlist'][0]['dissname'] + "-QQ"
+                for music in res['cdlist'][0]['songlist']:
+                    name = music["songname"]
+                    picUrl = ""
+                    artist = music["singer"][0]["name"]
+                    album = music["albumname"]
+                    search_url = f"{api_host}/cloudsearch?keywords={name}({artist})&limit=5&type=1"
+                    # songmid = music["songmid"]
+                    # url = f"https://lxmusicapi.onrender.com/url/tx/{songmid}/{qualityLevels['low']}"
+                    # headers = {"X-Request-Key": "share-v2"}
+                    try:
+                        response = requests.get(search_url, headers=headers)
+                        response.raise_for_status()  # 如果请求出现4xx、5xx等错误状态码则抛出异常
+                        print(name,artist)
+
+                        for song in response.json()['result']['songs']:
+                            if song['name'] == name and song['ar'][0]['name'] == artist:
+                                # print(song)
+                                url = f"{api_host}/song/url?id={song['id']}&br={br}&proxy=HTTP:%2F%2F127.0.0.1:8080"
+                                picUrl = song["al"]["picUrl"]
+                                break
+                    except requests.RequestException as e:
+                        log.info(f"getmy_playlist Error: Error fetching data for {name}: {e}")
+                        continue
+                    if (not name) or (not url):
+                        continue
+                    xiaomusic.all_music[name] = url
+                    xiaomusic.all_music_tags[name] = {
+                        "title": name,
+                        "artist": artist,
+                        "album": album,
+                        "year": "",
+                        "genre": "",
+                        "picture": picUrl,
+                        "lyrics": "",
+                    }
+                    one_music_list.append(name)
+                    if index > 100:
+                        break
+                    index += 1
+                log.debug(f"getmy_playlist name:{list_name}")
+                log.debug(one_music_list)
+                # 歌曲名字相同会覆盖
+                xiaomusic.music_list[list_name] = one_music_list
+                xiaomusic.try_save_tag_cache()
+        return
+
+    log.error(f"getmy_playlist type:{type} not support")
+
+
+if __name__ == '__main__':
+    getmy_playlist(api='qq',playlist_id=9381106378)
